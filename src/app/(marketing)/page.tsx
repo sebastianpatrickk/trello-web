c8bafaef7db553152996d8320a25710351379e36
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-02-11 18:01:59 +0100

    Update styling in MarketingPage component

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function MarketingPage() {
   return (
-    <section className="container flex max-w-6xl flex-col items-center pb-8 pt-6 md:py-8 ">
+    <section className="container flex max-w-6xl flex-col items-center pb-8 pt-6 md:py-8">
       <section className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-4 py-24 text-center md:py-32">
         <h1 className="text-balance text-3xl font-bold md:text-5xl">
           Trello brings all your tasks, teammates, and tools together
