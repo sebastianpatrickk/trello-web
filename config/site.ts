424d324eca9995e2125af808bb9b5f5628c8b7d8
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-02-11 17:33:07 +0100

    Add site configuration file

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+// IMPLEMENT-LATER: Revirite site configuration
+
+export const siteConfig = {
+  name: "Trello",
+  description:
+    "Trello’s boards, lists, and cards enable teams to organize and prioritize projects in a fun, flexible, and rewarding way.",
+};
