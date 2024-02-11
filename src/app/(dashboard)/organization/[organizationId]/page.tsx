7359e4d9da8c1a72e9b9a7b304dafc0bf67f2859
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-02-11 20:01:13 +0100

    Add organization ID page

---
+++ src/app/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+
+export default function OrganizationIdPage() {
+  const { userId, orgId } = auth();
+
+  return (
+    <div>
+      <OrganizationSwitcher hidePersonal />
+    </div>
+  );
+}
