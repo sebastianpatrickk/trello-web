a3e6a2d7c389ca55a943a3cd1da5e960b452af8e
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-02-11 17:25:25 +0100

    Add SiteHeader component

---
+++ src/components/layouts/site-header.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Logo from "../logo";
+import { buttonVariants } from "../ui/button";
+
+export default function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-20 w-full border-b bg-background">
+      {/* Mobile nav */}
+      <div className="container flex h-16 items-center">
+        <Logo />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-2">
+            <Link
+              className={cn("", buttonVariants({ variant: "link" }))}
+              href="/login"
+            >
+              Log in
+            </Link>
+            <Link className={buttonVariants()} href="/signup">
+              Get Trello for free
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
